fc03fc7d77a35296a9e46de604d73a4b8db5fe8e
Leonardo <leoded94@hotmail.com>
2016-06-07 15:47:37 -0500

    Prueba de Travis 16

--- Test.php
+++ Test.php
@@ -2,10 +2,8 @@
 include '\admin\controller\startup\login.php';
 class Test extends PHPUnit_Framework_TestCase
 {
-    index();
 	public function testOnePlusOne() {
 		$this->assertEquals(1+1,2);
   	}
-	
 }
 ?>
