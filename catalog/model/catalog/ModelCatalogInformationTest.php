15264a73891cc6670c575a2255e77745f1f0a5c1
Fernando Campana <fercamp09@gmail.com>
2016-06-15 21:43:36 -0500

    Remove image/no_image.png file

---
+++ catalog/model/catalog/ModelCatalogInformationTest.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Ecotel
+ * Date: 08/06/16
+ * Time: 0:01
+ */
+class ModelCatalogInformationTest extends PHPUnit_Framework_TestCase
+{
+    public function getInformationLayoutIdTest() {
+        $this->assertEquals(1,0);
+    }
+}
